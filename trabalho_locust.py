fcb5bac5ffbe7fbb5591cc5b074017b664267f45
Kauê Salazar <kauesalazarcardoso123@gmail.com>
2026-06-03 14:04:20 -0300

    Adiciona testes de performance com Locust

---
+++ trabalho_locust.py
@@ -0,0 +1,45 @@
+from locust import HttpUser, task, between
+
+class SauceDemoUser(HttpUser):
+wait_time = between(1, 3)
+
+
+@task(5)
+def home(self):
+    self.client.get("/")
+
+@task(4)
+def login(self):
+    self.client.post(
+        "/login",
+        json={
+            "username": "standard_user",
+            "password": "secret_sauce"
+        }
+    )
+
+@task(8)
+def produtos(self):
+    self.client.get("/inventory")
+
+@task(6)
+def adicionar_carrinho(self):
+    self.client.post(
+        "/cart/add",
+        json={"product_id": 1}
+    )
+
+@task(3)
+def visualizar_carrinho(self):
+    self.client.get("/cart")
+
+@task(2)
+def checkout(self):
+    self.client.post(
+        "/checkout",
+        json={
+            "firstName": "Teste",
+            "lastName": "Usuario",
+            "postalCode": "12345"
+        }
+    )
